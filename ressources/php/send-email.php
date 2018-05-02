66138ebfa3bfcaeb04830d9be969cf9fd0085b0b
Slinth <dorian.decroix@hotmail.fr>
2018-05-02 19:10:00 +0200

    [MAJ] Envoi mail formulaire contact
    - Utilisation de "Sendmailer" pour fonctionner sur Wamp
    - Fonctionnel
    - A tester sur serveur
    - Essayer avec une adresse Office 365

--- ressources/php/send-email.php
+++ ressources/php/send-email.php
@@ -2,7 +2,7 @@
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $tel = $_POST['tel'];
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL); //On accepte que les chaines étant des adresses valides = proctection contre le spam
+$email = filter_input(INPUT_POST, 'mail', FILTER_VALIDATE_EMAIL); //On accepte que les chaines étant des adresses valides = proctection contre le spam
 $mess = $_POST['message'];
 
 echo htmlspecialchars($nom);
@@ -11,7 +11,7 @@ echo htmlspecialchars($tel);
 echo htmlspecialchars($email);
 echo htmlspecialchars($mess);
 
-$mail = 'dorian.decroix@gmail.com'; // Déclaration de l'adresse de destination.
+$mail = $email; // Déclaration de l'adresse de destination.
 if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
 {
 	$passage_ligne = "\r\n";
@@ -21,7 +21,6 @@ else
 	$passage_ligne = "\n";
 }
 //=====Déclaration des messages au format texte et au format HTML.
-$message_txt = "Demande de contact de $nom $prenom";
 $message_html = "<html><head></head><body><b>Demande de contact</b> : $nom $prenom ($email | $tel)<br/>
                   <b>Message :</b> $mess</body></html>";
 //==========
@@ -53,14 +52,6 @@ $header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary
 $message = $passage_ligne."--".$boundary.$passage_ligne;
 $message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
 $message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
-//=====Ajout du message au format texte.
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_txt.$passage_ligne;
-//==========
-
-$message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
-
 //=====Ajout du message au format HTML.
 $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
 $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
