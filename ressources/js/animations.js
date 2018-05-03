b006b3a293cad11f95e6300ebac31876c01778ab
Slinth <dorian.decroix@hotmail.fr>
2018-05-03 12:14:38 +0200

    [MAJ] ALL (ProgressBar & Floating Button)

--- ressources/js/animations.js
+++ ressources/js/animations.js
@@ -8,10 +8,15 @@ $(window).on('load', function () {
 });
 
 $(window).scroll(function() {
-    if ($(this).scrollTop()){
-        $('#navbarBrand').addClass("small");
-    }
-    else{
-        $('#navbarBrand').removeClass("small");
+  if (document.getElementById('progress-bar') !== null) {
+    document.getElementById("progress-bar").max = $(document).height() - $(window).height();
+  }
+  var scrollValue = $(window).scrollTop()
+    if (scrollValue > 0) {
+      $('#progress-bar').val(scrollValue);
+      $('#navbarBrand').addClass("small");
+    } else {
+      $('#progress-bar').val(0);
+      $('#navbarBrand').removeClass("small");
     }
 });
