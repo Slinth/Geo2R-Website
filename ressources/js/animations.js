6ff72da9640cfc137ad4da1e1bb566727250bf68
Slinth <dorian.decroix@hotmail.fr>
2018-05-18 09:35:26 +0200

    [MAJ] Design Page Home
    - Animation au premier scroll : faire disparaitre la vidéo entièrement dès que scrollTop > 0
    - Test de réduction de temps de la vidéo + ajout du format Webm

--- ressources/js/animations.js
+++ ressources/js/animations.js
@@ -7,23 +7,38 @@ $(window).on('load', function () {
     AOS.refresh();
 });
 
+let scrolled = false;
+
 $(window).scroll(function() {
+  var scrollValue = $(window).scrollTop();
+
+  /* Pour les pages avec la progressBar (= tout sauf Accueil) */
   if (document.getElementById('progress-bar') !== null) {
     document.getElementById("progress-bar").max = $(document).height() - $(window).height();
   }
-  var scrollValue = $(window).scrollTop()
-    if (scrollValue > 0) {
-      $('#progress-bar').val(scrollValue);
-      $('#navbarBrand').addClass("small");
-    } else {
-      $('#progress-bar').val(0);
-      $('#navbarBrand').removeClass("small");
+  /* Pour la page Accueil */
+  else {
+    if (scrollValue > 0 && !scrolled) {
+      $('html, body').animate({
+        scrollTop: $("#navbar").offset().top
+      }, 500);
+      scrolled = true;
     }
+  }
+
+  /* Pour toutes les pages */
+  if (scrollValue > 0) {
+    $('#progress-bar').val(scrollValue);
+    $('#navbarBrand').addClass("small");
+  } else {
+    $('#progress-bar').val(0);
+    $('#navbarBrand').removeClass("small");
+  }
 });
 
 $(document).ready(function(){
-$('[data-toggle="popover"]').popover({
-placement : 'top',
-trigger : 'hover'
-});
+  $('[data-toggle="popover"]').popover({
+    placement : 'top',
+    trigger : 'hover'
+  });
 });
