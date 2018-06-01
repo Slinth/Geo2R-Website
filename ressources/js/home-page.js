b035237682b62c913286c4af7b6d1fddecccfa3c
Slinth <dorian.decroix@hotmail.fr>
2018-06-01 12:26:29 +0200

    [FIX] Modification Articles + Page Présentation

--- ressources/js/home-page.js
+++ ressources/js/home-page.js
@@ -1,5 +1,23 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    x[slideIndex-1].className += " show";
+    dots[slideIndex-1].className += " active";
+    setTimeout(carousel, 2300); // Change image every 2 seconds
+}
 
 // Next/previous controls
 function plusSlides(n) {
@@ -27,26 +45,3 @@ function showSlides(n) {
   slides[slideIndex-1].className += " show";
   dots[slideIndex-1].className += " active";
 }
-
-
-/*
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
-*/
