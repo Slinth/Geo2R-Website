6d3b1b079dd8d0c1eb8b865b350f8e378e44ac3d
Slinth <dorian.decroix@hotmail.fr>
2018-06-06 19:07:53 +0200

    [FIX] Corrections diverses

--- ressources/js/home-page.js
+++ ressources/js/home-page.js
@@ -1,13 +1,3 @@
-/*$('#videoId').on('loadstart', function (event) {
-   $(this).addClass('loading');
-   console.log("LOADING");
- });
- $('#videoId').on('canplay', function (event) {
-   $(this).removeClass('loading');
-   $(this).attr('poster', '');
-   console.log("END");
- });
-*/
 var slideIndex = 0;
 carousel();
 
